9e6e586372100af689c2354d282bff17fb1c96d5
Golon258 <golon338@gmail.com>
2024-06-01 16:07:47 +0200

    Add usage of viewsets and generic in app views

--- BlogApi/Blog/posts/urls.py
+++ BlogApi/Blog/posts/urls.py
@@ -1,11 +1,17 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(
+    "",  views.PostRetrieveViewSet, basename="posts"
+)
+
 
 
 urlpatterns = [
-    path("all/",                 views.PostListView.as_view(), name="list_posts"),
-    path("get/<int:post_id>",    views.PostRetrieveView.as_view(), name="post_detail+"),
-    path("create/",              views.PostCreateView.as_view(), name="create_post"),
-    path("update/<int:post_id>", views.PostUpdateView.as_view(), name="update_post"),
-    path("delete/<int:post_id>", views.PostRemoveView.as_view(), name="delete_post"),
+    path("retrieve/", include(router.urls)),
+    path("create/", views.PostCreateView.as_view(), name="create_post"),
+    path("update/<int:pk>", views.PostUpdateView.as_view(), name="update_post"),
+    path("delete/<int:pk>", views.PostRemoveView.as_view(), name="delete_post"),
 ]
